201e69c4610e1fc9d8835a38b9bc9c17bd0a2721
Andrew_Chapman <andrewbryce001@gmail.com>
2023-09-25 21:38:51 -0700

    Fun stuff

--- src/components/contact/ContactCard.js
+++ src/components/contact/ContactCard.js
@@ -1,6 +1,7 @@
 import InstaLogo from "../../photos/instagram (1).png"
 import LinkedinLogo from "../../photos/linkedin-logo.png"
 import GmailLogo from "../../photos/gmail.png"
+import GitLogo from "../../photos/github-mark.png"
 const ContactCard = () => {
 
     return (
@@ -9,8 +10,16 @@ const ContactCard = () => {
                 <h4 class ="title">CONTACT INFO</h4>
                 
                 <div class="contact_row">
+                    <a class="clickable_link" href= "https://www.linkedin.com/in/andrew-chapman-22031021b/" target="_blank">
                     <img src= {LinkedinLogo} class="logo"></img>
-                    <a href= "https://www.linkedin.com/in/andrew-chapman-22031021b/">Andrew Chapman</a>
+                    </a>
+                    <a class="clickable_link" href= "https://www.linkedin.com/in/andrew-chapman-22031021b/" target="_blank">Andrew Chapman</a>
+                </div>
+                <div class="contact_row">
+                    <a href= "https://github.com/acha2001" target="_blank">
+                        <img  src= {GitLogo} class="logo"></img>
+                    </a>
+                    <a href= "https://github.com/acha2001" target="_blank" class="clickable_link">acha2001</a>
                 </div>
                 <div class="contact_row">
                     <img src= {GmailLogo} class="logo"></img>
--- src/components/home/Name.js
+++ src/components/home/Name.js
@@ -3,7 +3,8 @@ const Name = () => {
   return (
     <>
         <div class="main_title main_item">
-          <h1>Andrew Chapman</h1>
+          <h1>Andrew Chapman
+          </h1>
           <text >Jr. Software Engineer</text>
         </div>
     </>
